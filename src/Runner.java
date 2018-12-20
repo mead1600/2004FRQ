26918a67ba4631b513c5abc7c0c09425bb466c8e
BT_1N3_06 <mgyawu0974@bths.edu>
2018-12-20 11:44:23 -0500

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args){
+        ArrayList animalArr = new ArrayList(Arrays.asList("cat","mouse","frog","dog","dog"));
+        
+    }
+}
